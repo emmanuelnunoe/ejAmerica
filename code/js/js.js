54f223615501d4b6376f0ec39abec63e37d8519c
emmanuelnunoe <39935404+emmanuelnunoe@users.noreply.github.com>
2020-10-08 23:32:45 -0700

    first commit
    advance in project

---
+++ code/js/js.js
@@ -0,0 +1,266 @@
+console.log("JS is working");
+
+// UABC | ejAmerica
+// 10-05-2020
+// Emmanuel Nuno Estrella
+// CRUD for user management 
+
+// USER  CONTROLLER
+var userCtrl = (function(){
+
+    var User = function(name, age, relocation,phone, email){
+        this.name = name;
+        this.age = age;
+        this.relocation = relocation;
+        this.phone = phone;
+        this.email = email;
+
+    }
+
+    var data = {
+      users:[]
+    }
+    User.prototype.getName = function() {
+        return this.name;
+    };
+
+    User.prototype.getAge = function(){
+        return this.age;
+    }
+    
+    User.prototype.getEmail = function(){
+        return this.email;
+    }
+
+    // generate random integer
+    var getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+    // generate random word for name
+    var randomWord = function(letters,except, plus){
+        var i,word,a,z,flag
+        a=97
+        z=122
+        i=0
+        word="s"
+
+        var randChar = String.fromCharCode(randInt); 
+        // setup exceptions
+        var exeptions =  except // example'[^*aoump""]'
+
+       while (i < letters && word.length < letters ) {
+
+        // select random int
+        var randInt = getRandomInt(a,z);
+        // convert int to char
+        var randChar = String.fromCharCode(randInt); 
+        // setup regular expresion
+        let regExpFirstWord= new RegExp(exeptions,flag)
+        // evaluate regular expresion
+        let result = regExpFirstWord.test(randChar)
+        // word builder after passing exceptions/RegExp
+            if( result )
+             {
+                 word+=randChar;
+                 i++
+             }
+  
+        }
+        if(plus)
+        {
+            randInt= getRandomInt(1,9)
+            word+=randInt
+        }
+      
+
+        return word;
+    
+    }
+
+    // random User
+    var randomUser = function(){
+            var rndName, rndAge, relocation, 
+                phone, email, usr,exception,
+                word1, word2, word3, word4
+            // generate random name.
+                exception = '[^*aoump""]'
+                word1 = randomWord(4,exception,false);
+                exception = '[*asdfghjklopuytem]'
+                word2 = randomWord(4,exception,false);
+                exception = '[^*aeou""]'
+                word3 = randomWord(4,exception,true);
+                exception = '[t]'
+                word4 = randomWord(4,exception,false);
+                rndName = word1+" "+word2+" "+word3+" "+word4
+            // generate random age.
+                rndAge = getRandomInt(18,46);
+            // identify relocation.
+                relocation = true
+            // generate random phone.
+                phone = randomPhone();
+                formatedPhone =  phoneFormat(phone)
+                console.log(formatedPhone)
+            // generate random email.
+                email = "myEmail@mail.com"
+            usr = new User(rndName,rndAge,relocation, phone,email);
+
+        return usr;
+    }
+
+    // generate random phone
+    var randomPhone = function(){
+        var phoneString,i 
+        phoneString=0
+        i=1
+        while ( i < 11){
+            var randInt = getRandomInt(1,10)
+            phoneString+=randInt.toString();
+            i++
+        }
+        return phone = parseInt(phoneString,10);
+    }
+    // display formated phone
+    var phoneFormat = function(number){
+        var phoneString
+        phoneString = number.toString()
+        return phoneString.replace(/(\d{1})(\d{3})(\d{2})(\d{2})(\d{2})/, "+$1 ($2) $3 $4 $5");
+    }
+
+    // edit user
+    // delete user
+    // delete all
+
+    // testing
+    var add = function(a){
+        var x = 5;
+        return x + a;
+    }
+
+    return {
+        // add a new user tor data strutcture
+        addNewUser: function(type, name, age, relocation, phone, email){
+            var newUser
+
+            if(type === "random"){
+                newUser = randomUser();
+            }else if( type === "input"){
+                newUser = new User(name,age,relocation,phone,email);
+            }
+
+            data.users.push(newUser);
+
+            return newUser;
+        },
+        // get all data from users
+        getData: function(){
+            return data; // TODO: return only specific information
+        },
+        clearData:function(){
+            return data.users=[]
+        },
+        // test for module
+        publicTest : function(){
+            return randomUser();
+        }
+
+    }
+
+
+
+})();
+
+
+// UICONTROLLER
+var UICtrl = (function(){
+    var DOMstrings ={
+    inputWord1 :'.word1',
+    inputBtn :'.saveButton'
+    }
+
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+    
+            
+    return {
+        getInput: function() {
+            return{
+                word1 : document.querySelector(DOMstrings.word1).value
+            };
+
+        },
+        getDOMstrings: function(){
+            return DOMstrings
+        },
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.name + ', ' +
+             DOMstrings.age + ','+ DOMstrings.relocation+','+ 
+             DOMstrings.phoneString+','+ DOMstrings.email);
+        },
+        displayUsers: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.name;
+
+        }
+    }
+
+
+
+})();
+
+// MAIN APP
+var controller = (function(userCtrl, UICtrl)
+{
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddUser)
+    }
+
+    var updateUsersList = function(){
+        var user = userCtrl.getUsers()
+        UICtrl.displayList(users)
+    }
+
+    var ctrlAddUser = function(){
+        var input, newUser;
+
+        // 1. Get the fields input data
+        input = UICtrl.getInput()
+        if (true){
+
+        // 2. Add the item to the user controller
+        newUser = userCtrl.addNewUser("input",input.word1+" "+input.word2+" "+input.word3,
+        input.age,true,input.phone,input.email )
+        
+        // 3. Add the item to the UI
+        UICtrl.addListItem(newUser);
+
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
+        // 5. update user list
+         updateUsersList();
+
+
+        }
+
+
+    }
+
+    return{
+
+        init: function(){
+            console.log("Aplication has started");
+            userCtrl.getData();
+            
+        }
+    }
+
+})(userCtrl,UICtrl);
+
+controller.init();
